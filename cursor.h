019c42f6ec0c17b57eeee9b83cf0872e99cc8084
shiningsstars <hanjjeongiee@gmail.com>
2026-03-17 17:07:01 +0700

    update header cursor

--- cursor.h
+++ cursor.h
@@ -1,14 +1,12 @@
 #ifndef CURSOR_H
 #define CURSOR_H
 
-#define MAX_ROW 100
-#define MAX_COL 100
+#define MAX_BARIS 100
 
-extern int cursor_row;
-extern int cursor_col;
+// extern supaya variabel ini bisa dibaca di main.c dan text-edit.c
+extern int cursor; 
 
-void moveLeft();
-void moveRight();
+int isCursorValid();
 void moveUp();
 void moveDown();
 
